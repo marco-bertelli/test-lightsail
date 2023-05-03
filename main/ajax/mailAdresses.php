ec76bba96f95549adf781a55ca06c350110c7b67
fourthree <pcremascoliweb@gmail.com>
2023-05-03 17:48:09 +0200

    Update mailAdresses.php
    me serve

--- main/ajax/mailAdresses.php
+++ main/ajax/mailAdresses.php
@@ -7,12 +7,14 @@
 	$address = "p.pasini@davittorio.com";      //indirizzo a cui deve andare la mail finale
 	$address2 = "f.gritti@davittorio.com";
 	$address3 = "v.teodori@davittorio.com";      //indirizzo a cui deve andare la mail finale
-	$address7 = "eventi@davittorio.com";  
+	$address7 = "eventi@davittorio.com";
+	$address8 = "s.marocchi@davittorio.com";
 	
 	$mail->AddAddress($address);
 	$mail->AddAddress($address2);
 	$mail->AddAddress($address3);
 	$mail->AddAddress($address7);
+	$mail->AddAddress($address8);
 	// $mail->AddAddress($address4);
 	// $mail->AddAddress($address5);
 	if ($inRange){
